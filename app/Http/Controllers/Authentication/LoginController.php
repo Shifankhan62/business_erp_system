a6edca54b66a37c3190a41afb2472bc55e99f5b4
Shifankhan62 <shifan.dws@gmail.com>
2026-08-02 16:46:04 +0530

    Add login view wiring and authentication branch updates

--- app/Http/Controllers/Authentication/LoginController.php
+++ app/Http/Controllers/Authentication/LoginController.php
@@ -10,7 +10,7 @@ class LoginController extends Controller
 {
     public function create()
     {
-        return view('authentication.login-controller');
+        return view('authentication.login');
     }
 
     public function store(Request $request)
